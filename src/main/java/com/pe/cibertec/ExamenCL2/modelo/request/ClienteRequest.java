12cc77c2732339be5e1c5cd7b054d07dd8f585e1
Bruno Aguilar <103317734+Cocobalde@users.noreply.github.com>
2023-05-28 22:10:24 -0500

    Add files via upload
    Subida del ClienteRequest

---
+++ src/main/java/com/pe/cibertec/ExamenCL2/modelo/request/ClienteRequest.java
@@ -0,0 +1,12 @@
+package com.pe.cibertec.ExamenCL2.modelo.request;
+
+import lombok.Data;
+
+public class ClienteRequest {
+	private Integer idCliente;
+	private String nombreCliente;
+	private String direccionCliente;
+	private String telefonoCliente;
+	private String correoCliente;
+	
+}
